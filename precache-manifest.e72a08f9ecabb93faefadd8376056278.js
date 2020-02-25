09055f511ae3816146d3ff0cb15f06bbdfa4baf9
Konstantine Berulava <berulava@gmail.com>
2020-02-26 01:20:50 +0400

    Add files via upload

---
+++ precache-manifest.e72a08f9ecabb93faefadd8376056278.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cba4f17bdb76593c9b0c7a252caf6da",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "dddc06f87b31ee91f806",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "6a080b08e6a3f64ac8eb",
+    "url": "/robofriends/static/css/main.42bbd0e2.chunk.css"
+  },
+  {
+    "revision": "dddc06f87b31ee91f806",
+    "url": "/robofriends/static/js/2.a6b4be5c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/robofriends/static/js/2.a6b4be5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a080b08e6a3f64ac8eb",
+    "url": "/robofriends/static/js/main.1fba1299.chunk.js"
+  },
+  {
+    "revision": "d6ceab57e5523008bfa1",
+    "url": "/robofriends/static/js/runtime-main.03e2f00f.js"
+  },
+  {
+    "revision": "557d66f33e23aeec8b068365cdd16535",
+    "url": "/robofriends/static/media/Starjedi.557d66f3.woff"
+  }
+]);
